6722bb82db8a7d579bec5b982d5e8dac57aa3ae7
Rob Cilissen <rob.cilissen-grassere@zuyd.nl>
2022-03-07 16:54:29 +0100

    Created business layer

---
+++ BU/Guest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayerExample.BU
+{
+    internal class Guest
+    {
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public Party Party { get; set; }
+
+        public Guest(string name, DateTime birthDate)
+        {
+            Name = name; 
+            BirthDate = birthDate; 
+        }
+    }
+}
---
+++ BU/Party.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayerExample.BU
+{
+    internal class Party
+    {
+        public string Name { get; private set; }
+        public DateTime StartingTime { get; set; }
+        public DateTime EndingTime { get; set; }
+        public int Budget { get; set; }
+        public List<Guest> Guests { get; set; } 
+
+        public Party(string name, DateTime startingTime, DateTime endingTime, int budget)
+        {
+            Name = name;
+            StartingTime = startingTime;
+            EndingTime = endingTime;
+            Budget = budget;
+            Guests = new List<Guest>();
+        }
+
+        public void AddGuest(Guest guest)
+        {
+            Guests.Add(guest);
+            guest.Party = this;
+        }
+    }
+}
